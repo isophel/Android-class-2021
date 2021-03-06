3d1890840f670abf5386f2fe33f557dab8b40fc4
Isophel <natisox18@gmail.com>
2021-03-06 16:45:09 +0300

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,8 @@
+package com.example.kotlinapp
+fun main(args: Array<String>){
+
+    var courselist: Array<String> = arrayOf("CSC3115 MobileProgramming, CSC3 Artificial Intelligence,CSC3113 Entreprenuership, CSC3104 Computer Networks and Security, CSC3112 Data Communications and Networks, CSC3117 Automata Complexity and Computability")
+
+    courselist.forEach{System.out.println(it)}
+
+}
